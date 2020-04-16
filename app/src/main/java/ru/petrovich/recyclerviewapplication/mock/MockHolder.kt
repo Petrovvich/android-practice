3ddd65007051e1afb5f5244374ce3c606645aaf5
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-04-16 21:36:41 +0700

    added decoration to list

--- app/src/main/java/ru/petrovich/recyclerviewapplication/mock/MockHolder.kt
+++ app/src/main/java/ru/petrovich/recyclerviewapplication/mock/MockHolder.kt
@@ -7,8 +7,8 @@ import ru.petrovich.list.view.yandex.map.recyclerviewapplication.R
 import ru.petrovich.recyclerviewapplication.listeners.OnItemClickListener
 
 class MockHolder(view: View) : RecyclerView.ViewHolder(view) {
-    private val mName = view.findViewById<TextView>(R.id.tv_name)
-    private val mValue = view.findViewById<TextView>(R.id.tv_value)
+    private val mName: TextView = view.findViewById(R.id.tv_name)
+    private val mValue: TextView = view.findViewById(R.id.tv_value)
     private lateinit var mId: String
 
     fun bind(mock: Mock) {
